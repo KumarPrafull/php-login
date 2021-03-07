2331b95c7d96f47d49248e8e70bf6c409fcb7998
Kumar Prafull <eprafull@gmail.com>
2021-03-07 11:58:50 +0530

    First Commit

---
+++ registration.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<title>User Registration | PHP</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<div>
+<?php
+if(isset($_POST['create'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $phonenumber = $_POST['phonenumber'];
+    $password = $_POST['password'];
+    
+
+    echo $firstname,  " " , $lastname,  " " , $email , " " , $phonenumber , " " , $password;
+}
+?>
+</div>
+
+<div>
+    <form action="registration.php" method="post">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h1>Registration</h1>
+                    <p>Fill up the form with correct value</p>
+
+                    <hr class="mb-3">
+                    <label for="firstname"><b>First Name</b></label>
+                    <input type="text" name="firstname" required>
+
+                    <label for="lastname"><b>Last Name</b></label>
+                    <input type="text" name="lastname" required>
+
+                    <label for="email"><b>Email Address</b></label>
+                    <input type="email" name="email" required>
+
+                    <label for="phonenumber"><b>Phone Number</b></label>
+                    <input type="text" name="phonenumber" required>
+
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" name="password" required>
+                    <hr class="mb-3">
+                    
+                    <input class="btn btn-primary" type="submit" name="create" value="Sign Up"/>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html> 
